90efa3d0bb1f1f16f0d03a3605ffd992f5731623
unknown <cristian.forero-m@labinfo.is.escuelaing.edu.co>
2022-09-07 18:07:58 -0500

    Puerto arreglado

--- src/main/java/co/edu/escuelaing/myspring/MySpring.java
+++ src/main/java/co/edu/escuelaing/myspring/MySpring.java
@@ -36,7 +36,7 @@ public class MySpring {
         boolean running = true;
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -86,6 +86,13 @@ public class MySpring {
         }
         serverSocket.close();
 
+    }
+    
+    private static int getPort(){
+        if (System.getenv("PORT") != null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35000;
     }
 
 }
